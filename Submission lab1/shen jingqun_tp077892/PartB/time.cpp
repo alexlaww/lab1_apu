2ee621071c55c35f3afc41aa387a3e7993feff25
luckyxiao6J <2117247034@qq.com>
2025-03-25 15:25:23 +0800

    PartB

---
+++ Submission lab1/shen jingqun_tp077892/PartB/time.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+// 验证输入格式
+bool isValidTime(int hh, int mm, int ss) {
+    return (hh >= 0 && hh < 24) && (mm >= 0 && mm < 60) && (ss >= 0 && ss < 60);
+}
+
+int main() {
+    string input;
+    int hh, mm, ss;
+    char colon1, colon2;
+
+    while (true) {
+        cout << "Please enter your elapsed time (in HH:MM:SS format) = ";
+        cin >> input;
+
+        stringstream ssInput(input);
+        if (ssInput >> hh >> colon1 >> mm >> colon2 >> ss && colon1 == ':' && colon2 == ':' && isValidTime(hh, mm, ss)) {
+            break;
+        } else {
+            cout << "Invalid input!" << endl;
+        }
+    }
+
+    int elapsedSeconds = hh * 3600 + mm * 60 + ss;
+    cout << "Elapsed time in seconds = " << elapsedSeconds << " seconds." << endl;
+
+    return 0;
+}
