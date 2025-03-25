6b12bb1927d0065102aba969f990f0f3415d70cf
Jylim00 <limjiayuan1115@gmail.com>
2025-03-25 23:57:52 +0800

    change

---
+++ submission 25032025/Lim Jia Yuan TP078560/Class Activity/Chap 3/Chap3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+//Class Activity 1:
+// int main()
+// {
+//     int billy [] = {16 ,2, 77, 40, 12071, 51};
+//     int sum = 0; 
+//     int min = billy[0];
+//     int max = billy[0];
+
+//     for (int i = 0; i < 6; i++)
+//     {
+//         sum += billy[i];
+
+//         if (billy[i] < min)
+//         {
+//             min = billy[i];
+//         }
+
+//         if (billy[i] > max)
+//         {
+//             max = billy[i];
+//         }
+//     }
+
+//     cout << "Sum of given array: " << sum << endl;
+//     cout << "Minimum Value: " << min << endl;
+//     cout << "Maximum Value: " << max << endl;
+
+//     return 0;
+// }
+
+//Class Activity 2:
+// int main()
+// {
+//     int array[5];
+//     int sum = 0;
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         cout << "Enter your number " << i + 1 << ":" ;
+//         cin >> array[i];
+//         sum += array[i];
+//     }
+
+//     cout << "The sum of the numbers is: ";
+//     for (int i = 0; i < 5; i++)
+//     {
+//         cout << array[i];
+
+//         if (i != 4)
+//         {
+//             cout << " + ";
+//         }
+//     }
+
+//     cout << " = " << sum << endl;
+
+//     return 0;
+// }
