103b20451cebaa3c60ae0924a51e951228816bab
Islam-14 <TP076756@mail.apu.edu.my>
2025-03-27 09:14:11 +0800

    My Class Submission

---
+++ Submission 27032025/TP076756/Lecture 2/index.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+/*const int NUMBER = 12;*/
+
+int main(){
+
+    cout<<3;
+    int y;
+    if(y>0){
+        int x = 1;
+        cout<<"The result is: "<<x;
+    }
+    /*int feet = 9;
+    int lenght;
+    int inch;
+    double centimeters;
+
+    cout<<"please enter your lenght: ";
+    cin>>lenght;
+
+    lenght = feet*12;
+
+    cout<<"Please enter your feet: ";
+    cin>>feet;
+    
+    cout<<"please enter your inch: ";
+    cin>>inch;
+
+    cout<<"please enter your centimeters: ";
+    cin>>centimeters;*/
+
+
+    
+    return 0;
+}
