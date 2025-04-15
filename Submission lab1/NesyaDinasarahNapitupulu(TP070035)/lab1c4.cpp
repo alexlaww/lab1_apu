6a9ff9bb2a66553c0b209e1fe0e670572d5e649e
dnsya <dn.nesya@gmail.com>
2025-04-15 21:46:14 +0800

    adjustment on lab 1 c4

--- Submission lab1/NesyaDinasarahNapitupulu(TP070035)/lab1c4.cpp
+++ Submission lab1/NesyaDinasarahNapitupulu(TP070035)/lab1c4.cpp
@@ -6,21 +6,21 @@ int main() {
     cout << "Enter number of rows (for diamond (triangle) dimension): ";
     cin >> row;
 
-    for (int i = 1; i <= row; i++) {
-        for (int j = i; j < row; j++) {
+    for (int i = 1; i <= row; ++i) {
+        for (int j = 1; j <= row - i; ++j) {
             cout << " ";
         }
-        for (int j = 1; j <= (2 * i - 1); j++) {
+        for (int j = 1; j <= (2 * i - 1); ++j) {
             cout << "*";
         }
         cout << endl;
     }
 
-    for (int i = row - 1; i >= 1; i++) {
-        for (int j = row; j > i; j--) {
+    for (int i = row - 1; i >= 1; --i) {
+        for (int j = 1; j <= row - i; ++j) {
             cout << " ";
         }
-        for (int j = 1; j <= (2 * i - 1); j++) {
+        for (int j = 1; j <= (2 * i - 1); ++j) {
             cout << "*";
         }
         cout << endl;
